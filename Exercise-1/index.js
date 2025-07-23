dd8aaed5c6ec770a819ac573c6ee922623a6615b
cristian <cristianpglez0@gmail.com>
2025-07-23 23:24:45 +0200

    add exercise-1

---
+++ Exercise-1/index.js
@@ -0,0 +1,14 @@
+let myFavoriteSuperhero = "Hulk";
+
+let favoriteNumber = 50;
+
+let height = 5;
+let length = 10;
+
+let addition = height + length;
+
+console.log("My favorite superhero is " + myFavoriteSuperhero);
+console.log("My favorite number is " + favoriteNumber);
+console.log("The height is " + height + " and the length is " + length);
+console.log("The result of the addition is " + addition);
+console.log("All variables are defined and logged successfully.");
