f5277b29a1af6ec0ce53a1b18b773a51e6f4d386
cristian pereiro <cristianpglez0@gmail.com>
2025-07-24 19:15:20 +0200

    add Exercise 4

---
+++ Exercise-4/index.js
@@ -0,0 +1,23 @@
+const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
+
+
+console.log(aldeanos[3]
+);
+
+aldeanos[4] = "Cervasio"
+console.log(aldeanos[4]);
+
+
+aldeanos[0] = "Bambina";
+
+console.log(aldeanos);
+
+console.log(aldeanos.reverse());
+
+
+
+aldeanos.splice(3, 3, "Canela");
+
+console.log(aldeanos);
+
+console.log(aldeanos[aldeanos.length - 1]);
