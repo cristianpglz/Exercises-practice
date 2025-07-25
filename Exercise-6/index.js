efe836cc335c8ac08bf70354230c446dfab27526
cristian pereiro <cristianpglez0@gmail.com>
2025-07-25 17:57:59 +0200

    add Exercise-6

---
+++ Exercise-6/index.js
@@ -0,0 +1,22 @@
+for (i = 0; i < 10; i++){
+  console.log(i);
+}
+
+
+
+for (i = 0; i < 10; i++){
+ if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+
+
+for (ovejas = 0; ovejas < 10; ovejas++){
+    if (ovejas < 9) {
+        console.log('Intentando dormir 🐑');
+        
+    }else {
+        console.log('Dormido 💤');
+    }
+}
