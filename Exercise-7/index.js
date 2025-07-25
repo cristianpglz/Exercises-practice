ddd2d1eb4c244e6b3f53f0e278a871e362721445
cristian pereiro <cristianpglez0@gmail.com>
2025-07-25 18:11:08 +0200

    add Exercise-7

---
+++ Exercise-7/index.js
@@ -0,0 +1,12 @@
+
+
+function greaterNumber(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  }
+    return numberTwo;
+}
+let numberOne = 70;
+let numberTwo = 10;
+console.log(greaterNumber(numberOne, numberTwo));
+
