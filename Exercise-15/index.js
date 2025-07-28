a03d6dcb80128538ae575289b72b1743a18297b4
cristian pereiro <cristianpglez0@gmail.com>
2025-07-29 00:13:21 +0200

    add Exercise 14

---
+++ Exercise-15/index.js
@@ -0,0 +1,25 @@
+const products = [
+  "Camiseta de Metallica",
+  "Pantalón vaquero",
+  "Gorra de beisbol",
+  "Camiseta de Basket",
+  "Cinturón de Orión",
+  "AC/DC Camiseta",
+];
+
+function productFinder(productList, productParam) {
+  console.log(`Buscando el producto: ${productParam}`);
+   let items = {};
+  for (let i = 0; i < productList.length; i++) {
+    if (productList[i].includes(productParam)) {
+        items[productList[i]] = (items[productList[i]] || 0) + 1;
+       }
+  }
+  return items;
+    
+    
+  
+  
+  
+}
+console.log(productFinder(products, "Camiseta"));
