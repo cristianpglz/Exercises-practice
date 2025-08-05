dd272a8bdbe7141239128d888d6caa1759c6cc4c
cristian pereiro <139070698+cristianpglz@users.noreply.github.com>
2025-08-05 22:53:00 +0200

    add Exercise-27

---
+++ Exercise-27/index.js
@@ -0,0 +1,24 @@
+const cartoons = [
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+];
+
+function getOldestCartoon(cartoonsList) {
+    let oldest = cartoonsList[0];
+    for (const cartoon of cartoonsList) {
+        if (cartoon.debut < oldest.debut) {
+            oldest = cartoon;
+        }
+    }
+    return oldest;
+}
+
+console.log(getOldestCartoon(cartoons));
