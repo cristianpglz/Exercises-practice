385a5f05e95f1fe986023cfd106169f9268d46cd
cristian pereiro <cristianpglez0@gmail.com>
2025-08-12 21:54:00 +0200

    add Exercise-39

---
+++ Exercise-39/index.js
@@ -0,0 +1,51 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+function userFavoritesSounds(users) {
+  const uniqueSounds = [];
+  for (const user of users) {
+    for (const sound in user.favoritesSounds) {
+      if (!uniqueSounds.includes(sound)) {
+        uniqueSounds.push(sound);
+      }
+    }
+  }
+  console.log("Sonidos únicos:", uniqueSounds);
+}
+
+
+
+
+userFavoritesSounds(users);
